512b060e411f509813363a4084cfc6d761f0118b
puni9869 <punitinani1@hotmail.com>
2024-06-09 15:08:49 +0530

    chore: adding logo and rename readmd file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mark/',
+  base: '/blabla-page/',
   plugins: [vue()],
   configureWebpack: {
         resolve: {
