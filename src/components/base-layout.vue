99ea177dccee154c38a6c59fbd3c4aa6ae1d6b2f
puni9869 <punitinani1@hotmail.com>
2024-06-10 12:15:26 +0530

    feat(add):  greatings

--- src/components/base-layout.vue
+++ src/components/base-layout.vue
@@ -1,3 +1,12 @@
 <template>
+  <div class="flex" style="align-items: center; justify-content: flex-start;font-style: italic;color: gray; padding-left: 0.625rem; "> Enjoy the rest of your {{day}}! </div>
   <div class="min-w-screen min-h-screen flex"><slot /></div>
 </template>
+
+<script setup> 
+import { ref } from 'vue';
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const d = new Date();
+const day = ref(weekday[d.getDay()]);
+</script>
