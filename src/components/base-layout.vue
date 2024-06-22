684f22141cdff557929f870c0bf44193392c614e
puni9869 <punitinani1@hotmail.com>
2024-06-22 22:27:51 +0530

    chore: removing unused code and libs from package json

--- src/components/base-layout.vue
+++ src/components/base-layout.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="flex" style="align-items: center;font-style: italic;color: gray; padding-left: 0.625rem; "> Enjoy the rest of your {{day}}! </div>
+  <div class="flex greetings">
+    Enjoy the rest of your {{ day }}!
+    <a class="flex-1" href="https://github.com/puni9869/blabla-page">
+      <img alt="blabla.page" class="flex github" :src="getImageUrl()" />
+    </a>
+  </div>
   <div class="min-w-screen min-h-screen flex"><slot /></div>
 </template>
 
-<script setup> 
-import { ref } from 'vue';
+<script setup>
+import { ref } from "vue";
 
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 const d = new Date();
 const day = ref(weekday[d.getDay()]);
+function getImageUrl() {
+  return new URL(`../images/icons/github_icon.ico`, import.meta.url).href;
+}
 </script>
+<style scoped>
+.github {
+  align-items: end;
+  color: gray;
+  width: 28px;
+  height: 28px;
+  margin-left: auto;
+  cursor: pointer;
+}
+.greetings {
+  align-items: center;
+  font-style: italic;
+  color: gray;
+  padding-left: 0.625rem;
+}
+</style>
