baaee436030ae8f33bac82f0bbd8864345583d1a
puni9869 <punitinani1@hotmail.com>
2024-06-22 23:26:05 +0530

    chore: adding blabla.page chrome extension placeholder and adding icon.

--- src/components/base-layout.vue
+++ src/components/base-layout.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="flex greetings">
-    Enjoy the rest of your {{ day }}!
-    <a target="_blank" class="flex-1" href="https://github.com/puni9869/blabla-page">
-      <img alt="blabla.page" class="flex github" :src="getImageUrl()" />
+  <div class="flex">
+    <span class ="greetings">Enjoy the rest of your {{ day }}!</span>
+    <span class="flex ml-auto">
+    <a target="_blank" href="https://github.com/puni9869/blabla-page">
+      <img title="github.com/puni9869/blabla-page" alt="blabla.page" class="flex icon" :src="getImageUrl('github_icon.ico')" />
     </a>
+    <a target="_blank" href="https://github.com/puni9869/blabla-page">
+      <img alt="Add to chrome"  title="Download the chrome extension" class="flex icon extension" :src="getImageUrl('chrome.png')" />
+    </a>
+  </span>
   </div>
   <div class="min-w-screen min-h-screen flex"><slot /></div>
 </template>
@@ -22,13 +27,14 @@ const weekday = [
 ];
 const d = new Date();
 const day = ref(weekday[d.getDay()]);
-function getImageUrl() {
-  return new URL(`../images/icons/github_icon.ico`, import.meta.url).href;
+function getImageUrl(iconName) {
+  return new URL(`../images/icons/${iconName}`, import.meta.url).href;
 }
 </script>
 <style scoped>
-.github {
+.icon {
   align-items: end;
+  gap: 1;
   color: gray;
   width: 28px;
   height: 28px;
@@ -40,5 +46,10 @@ function getImageUrl() {
   font-style: italic;
   color: gray;
   padding-left: 0.625rem;
+}
+.extension {
+  width: 24px;
+  height: 24px;
+  margin: 2px;
 }
 </style>
